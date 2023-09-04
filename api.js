5a9e635a86a0b84cc16a891adacfe237d399bcd9
ccalderon117 <51422577+ccalderon117@users.noreply.github.com>
2023-09-04 11:16:25 -0500

    Initial commit

---
+++ api.js
@@ -0,0 +1,77 @@
+import { initializeApp } from "firebase/app"
+import {
+    getFirestore,
+    collection,
+    doc,
+    getDocs,
+    getDoc,
+    query,
+    where,
+    documentId
+} from "firebase/firestore/lite"
+
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDUWH4BiF8NtCNSYn3SoN7lqCeJGsZ9CuA",
+  authDomain: "vanlife-fa45f.firebaseapp.com",
+  projectId: "vanlife-fa45f",
+  storageBucket: "vanlife-fa45f.appspot.com",
+  messagingSenderId: "325533746237",
+  appId: "1:325533746237:web:0c077d73a45756044dd654"
+};
+
+
+
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
+
+// Refactoring the fetching functions below
+const vansCollectionRef = collection(db, "vans")
+
+export async function getVans() {
+    const snapshot = await getDocs(vansCollectionRef)
+    const vans = snapshot.docs.map(doc => ({
+        ...doc.data(),
+        id: doc.id
+    }))
+    return vans
+}
+
+export async function getVan(id) {
+    const docRef = doc(db, "vans", id)
+    const snapshot = await getDoc(docRef)
+    return {
+        ...snapshot.data(),
+        id: snapshot.id
+    }
+}
+
+export async function getHostVans() {
+    const q = query(vansCollectionRef, where("hostId", "==", "123"))
+    const snapshot = await getDocs(q)
+    const vans = snapshot.docs.map(doc => ({
+        ...doc.data(),
+        id: doc.id
+    }))
+    return vans
+}
+
+
+export async function loginUser(creds) {
+    const res = await fetch("/api/login",
+        { method: "post", body: JSON.stringify(creds) }
+    )
+    const data = await res.json()
+
+    if (!res.ok) {
+        throw {
+            message: data.message,
+            statusText: res.statusText,
+            status: res.status
+        }
+    }
+
+    return data
+}
